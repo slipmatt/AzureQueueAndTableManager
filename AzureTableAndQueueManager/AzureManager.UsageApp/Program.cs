14aa4502369c82b8073537578ef44fbe4a5e3510
Imtiaz Buckas <imtiaz.pocketpc@gmail.com>
2017-08-27 18:19:55 +0200

    Init Commit, queues

---
+++ AzureTableAndQueueManager/AzureManager.UsageApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AzureManager;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace AzureManager.UsageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueManager.Initialize();
+            QueueManager.AddMessage("test1");
+            QueueManager.AddMessage("Test2");
+            Console.WriteLine(QueueManager.PeekMessage());
+            ShowMessages();
+            DeleteMessages();
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        private static void DeleteMessages()
+        {
+            var messageToDelete = QueueManager.GetMessage();
+            var count = 0;
+            while (count <= 5)
+            {
+                messageToDelete = QueueManager.GetMessage();
+                Console.WriteLine("No Message...Sleeping count: " + count);
+                Thread.Sleep(1000);
+                count++; //FailSafe
+                if (messageToDelete != null)
+                {
+                    Console.WriteLine("Deleted message: " + QueueManager.DeleteMessage(messageToDelete).ToString());
+                    count = 0;
+                }
+            }
+        }
+
+        private static void ShowMessages()
+        {
+            var Messages = QueueManager.GetMessages();
+            Console.WriteLine("Messages");
+            foreach (var message in Messages)
+            {
+                Console.WriteLine(message.AsString);
+            }
+        }
+    }
+}
---
+++ AzureTableAndQueueManager/AzureManager/QueueManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureManager.Properties;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+using System.Configuration;
+
+namespace AzureManager
+{
+    public static class QueueManager
+    {
+        private static CloudStorageAccount _storageAccount;
+        private static CloudQueueClient _queueClient;
+        private static CloudQueue _queue;
+
+        public static void Initialize()
+        {
+            _storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            _queueClient = _storageAccount.CreateCloudQueueClient();
+            CreateQueueIfNotExist();
+        }
+
+        private static void CreateQueueIfNotExist()
+        {
+            // Retrieve a reference to a container.
+            _queue = _queueClient.GetQueueReference(ConfigurationManager.AppSettings["QueueName"]);
+
+            // Create the queue if it doesn't already exist
+            _queue.CreateIfNotExists();
+        }
+
+        public static Task AddMessage(string Message)
+        {
+
+            _queue = _queueClient.GetQueueReference(ConfigurationManager.AppSettings["QueueName"]);
+            CloudQueueMessage message = new CloudQueueMessage(Message);
+            // Add the message to the queue
+            return _queue.AddMessageAsync(message);
+        }
+
+        public static string PeekMessage()
+        {
+            return _queue.PeekMessage().AsString;
+        }
+
+        public static CloudQueueMessage GetMessage()
+        {
+            return _queue.GetMessage();
+        }
+
+        public static List<CloudQueueMessage> GetMessages()
+        {
+            return _queue.GetMessages(10, System.TimeSpan.FromSeconds(1)).ToList();
+        }
+
+        public static string DeleteMessage(CloudQueueMessage message)
+        {
+            var deletedMessage = message.AsString;
+            _queue.DeleteMessage(message);
+            return deletedMessage;
+        }
+    }
+}
